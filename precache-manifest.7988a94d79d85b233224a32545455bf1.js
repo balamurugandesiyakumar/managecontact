bc442215a40805be969e649ceb862facfbd9986a
Balamurugan-d <balamurugan.d@ssi.safestart.com>
2020-04-14 10:37:58 +0530

    Updates

---
+++ precache-manifest.7988a94d79d85b233224a32545455bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130d0a797826cea8358f3585f9acc7d8",
+    "url": "/managecontact/index.html"
+  },
+  {
+    "revision": "e946b6362d74ca400738",
+    "url": "/managecontact/static/css/main.5694d2b8.chunk.css"
+  },
+  {
+    "revision": "4999d8db871880dd047a",
+    "url": "/managecontact/static/js/2.c5618ab8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/managecontact/static/js/2.c5618ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e946b6362d74ca400738",
+    "url": "/managecontact/static/js/main.891f2401.chunk.js"
+  },
+  {
+    "revision": "4e1076521459b19fff99",
+    "url": "/managecontact/static/js/runtime-main.dc7029aa.js"
+  }
+]);
